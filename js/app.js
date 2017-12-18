1ccb0a6535ce501d7f1501e405aaba4ff12aed51
simplon <dekorte.tsrit@gmail.com>
2017-12-18 12:43:59 +0100

    ma solution

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 (function main(){
-	
+
+	$('#idbtn').click(function(){
+		testWhile();
+	});
+
+	function testWhile(){
+		var chaine="";
+
+		do { 
+			chaine=prompt('saisir text avec le caractère " :', 'text ici');
+			}
+		while (chaine.indexOf('"')==-1)    
+			alert('La chaine entrée est "'+chaine+'"');
+	}
+
 })();
